a1a84d0d705cc94a7f36fa091843ca32aa734d1a
oğulcan <123860363+prmdogi2@users.noreply.github.com>
2025-11-17 16:48:04 +0300

    ödev3

--- js/task-1.js
+++ js/task-1.js
@@ -1,8 +1,8 @@
-function slug(title) {
+function slugify(title) {
   return title.toLowerCase().split(" ").join("-");
 }
 
-console.log(slug("Arrays for begginers")); // "arrays-for-begginers"
-console.log(slug("English for developer")); // "english-for-developer"
-console.log(slug("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
-console.log(slug("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
+console.log(slugify("Arrays for begginers")); // "arrays-for-begginers"
+console.log(slugify("English for developer")); // "english-for-developer"
+console.log(slugify("Ten secrets of JavaScript")); // "ten-secrets-of-javascript"
+console.log(slugify("How to become a JUNIOR developer in TWO WEEKS")); // "how-to-become-a-junior-developer-in-two-weeks"
